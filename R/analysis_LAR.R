8e2a2f9580b96e51c64c348ab70bf98ead8f7321
ShunHasegawa <hasegawashun.virgo@googlemail.com>
2017-09-08 14:32:15 +0900

    dominant and subordinate spp were analysed

--- R/analysis_LAR.R
+++ R/analysis_LAR.R
@@ -1,200 +1,238 @@
 
 # prepare data frame ------------------------------------------------------
 
-
-# Compute total log annual change ratios (LAR) for C3 and C4 graminoids, and
-# merge with environmental variables
+# Compute total log annual rate of chnage (LAR) in C3 and C4 graminoids for
+# dominant and subordiante spp, and merge with environmental variables
 
 env_data <- env_data %>% 
   mutate(ring = as.character(ring), 
          plot = as.character(plot))
 
-lar_data <- graminoid_pfg_df %>% 
-  mutate(ring = as.character(ring), plot = as.character(plot)) %>% 
+
+# compute loag annual rates of change for each group
+lar_data <- graminoid_DS %>%
+  mutate(type_pfg = paste(type, pfg, sep = "_")) %>% 
+  select(-pfg, -type) %>% 
   arrange(variable, ring, plot, year) %>%
-  mutate(value = value + 1) %>%                 # some speceis were absent, so add 1
-  group_by(co2, ring, plot, pfg, variable) %>%  
-  mutate(lar = log(value / lag(value, 1))) %>%  # comput LAR for each species
+  mutate(value = value + 1) %>%                                              # some speceis were absent, so add 1
+  group_by(co2, ring, plot, type_pfg, variable) %>%  
+  mutate(lar = log(value / lag(value, 1))) %>%                               # comput LAR for each species
   filter(year != "Year0") %>%
-  group_by(year, co2, ring, plot, pfg) %>% 
-  summarise(total_lar = sum(lar)) %>%           # compute total LAR for each PFG
+  group_by(year, co2, ring, plot, type_pfg) %>% 
+  summarise(total_lar = sum(lar)) %>%                                        # compute total LAR for each type_pfg
   ungroup() %>% 
-  spread(key = pfg, value = total_lar) %>% 
-  rename(lar_c3 = c3, lar_c4 = c4) %>% 
+  spread(key = type_pfg, value = total_lar) %>% 
+  rename(lar_dc3 = D_c3, lar_sc3 = S_c3, lar_dc4 = D_c4, lar_sc4 = S_c4) %>% 
   left_join(env_data) %>% 
-  mutate(s_lar_c3    = scale(lar_c3)[, 1],         # Z-standardize numeric variables
-         s_lar_c4    = scale(lar_c4)[, 1],
-         s_logmoist  = scale(log(Moist))[, 1],
-         s_temp      = scale(Temp)[, 1],
-         s_logpar    = scale(log(PAR))[, 1])
+  mutate(s_lar_dc3  = scale(lar_dc3)[, 1],                                   # Z-standardize numeric variables
+         s_lar_sc3  = scale(lar_sc3)[, 1],
+         s_lar_dc4  = scale(lar_dc4)[, 1],
+         s_lar_sc4  = scale(lar_sc4)[, 1],
+         s_logmoist = scale(log(Moist))[, 1],
+         s_temp     = scale(Temp)[, 1],
+         s_logpar   = scale(log(PAR))[, 1])
+
+
 
 
 # analysis  ------------------------------------------------------------
 
-options(na.action = "na.fail")
+options(na.action = "na.fail") # chnage na.action setting to use MuMIn::dredge
+xvars <- c("s_logpar", "s_logmoist", "s_temp", "co2")
 
 
-# > LAR_C4 ----------------------------------------------------------------
+
+
+# > dominant c3  -------------------------------------------------------------
 
 
 # test interactions
-lar_c4_m1 <- lmer(s_lar_c4 ~ co2 * (s_logmoist+s_temp+s_logpar) + 
-                    (1|ring)+(1|ring:plot)+(1|ring:year), data = lar_data)
-plot(lar_c4_m1)
-qqPlot(resid(lar_c4_m1))
-mcp.fnc(lar_c4_m1)
-oldf_c4 <- romr.fnc(lar_c4_m1, data = data.frame(lar_data))
-dplyr::setdiff(oldf_c4$data0, oldf_c4$data)
-  # one outlier is indicated
-
-
-# remove the outlier
-lar_c4_m2 <- update(lar_c4_m1, data = oldf_c4$data)
-plot(lar_c4_m2)
-qqPlot(resid(lar_c4_m2))
-mcp.fnc(lar_c4_m2)
-
-
-dredge(lar_c4_m2, REML = F, fixed = c("co2", "s_logmoist", "s_temp", "s_logpar"))
-dredge(lar_c4_m2, REML = F)
-  # no interaction is suggested
+lar_dc3_m1 <- lmer(s_lar_dc3 ~ co2 * (s_logmoist + s_temp + s_logpar) + 
+                     (1|ring) + (1|ring:year) + (1|ring:plot), data = lar_data)
+plot(lar_dc3_m1)
+qqPlot(resid(lar_dc3_m1))
+mcp.fnc(lar_dc3_m1)
+oldf_dc3 <- romr.fnc(lar_dc3_m1, data = data.frame(lar_data))
+dplyr::setdiff(oldf_dc3$data0, oldf_dc3$data)
+# some outliers are suggested, so remove them
+lar_dc3_m2 <- update(lar_dc3_m1, data = oldf_dc3$data)
+plot(lar_dc3_m2)
+qqPlot(resid(lar_dc3_m2))
+lar_dc3_m1_full <- dredge(lar_dc3_m1, fixed = c("co2", "s_logmoist", "s_temp",  "s_logpar"))
+lar_dc3_m2_full <- dredge(lar_dc3_m2, fixed = c("co2", "s_logmoist", "s_temp",  "s_logpar"))
+lar_dc3_m1_full
+lar_dc3_m2_full
+# no interaction is indicated either way
 
 
 # test main effects
-lar_c4_m3 <- lmer(s_lar_c4 ~ co2 + s_logmoist + s_temp +  s_logpar + 
-                    (1|ring)+(1|ring:plot)+(1|ring:year), data = oldf_c4$data)
+lar_dc3_m3 <- lmer(s_lar_dc3 ~ co2 + s_logmoist+s_temp + s_logpar +
+                     (1|ring) + (1|ring:year) + (1|ring:plot), data = lar_data)
+lar_dc3_m4 <- lmer(s_lar_dc3 ~ co2 + s_logmoist+s_temp + s_logpar +
+                     (1|ring) + (1|ring:year) + (1|ring:plot), data = oldf_dc3$data)
 
 
 # model diagnosis
-plot(lar_c4_m3)
-qqPlot(resid(lar_c4_m3))
-mcp.fnc(lar_c4_m3)
+plot(lar_dc3_m3)
+qqPlot(resid(lar_dc3_m3))
+plot(lar_dc3_m4)
+qqPlot(resid(lar_dc3_m4))
+
+summary(lar_dc3_m3)
+summary(lar_dc3_m4)
+# no difference so present the first one without removing the outliers
 
 
 # get 95% and 90% CI for coefficients
-summary(lar_c4_m3)
-r.squaredGLMM(lar_c4_m3)
-confint(lar_c4_m3, method = "boot", nsim = 999)
-confint(lar_c4_m3, method = "boot", level = .9, nsim = 999)
+summary(lar_dc3_m3)
+r.squaredGLMM(lar_dc3_m3)
+confint(lar_dc3_m3, method = "boot", nsim = 999)
+confint(lar_dc3_m3, method = "boot", level = .9, nsim = 999)
 
 
 # get relative importance of each predictor from a models set composed of models
 # with all possible combination of each predictor
-lar_c4_m3_all <- dredge(lar_c4_m3, REML = F)
-lar_c4_m3_all
-importance(lar_c4_m3_all)
-  # ensure the total number of occurence of each predictor is equal
+lar_dc3_m3_all <- dredge(lar_dc3_m3, REML = F)
+lar_dc3_m3_all
+importance(lar_dc3_m3_all)
+# ensure the total number of occurence of each predictor is equal
 
 
 # partial regression plot
-xvars <- c("co2", "s_logmoist", "s_temp", "s_logpar")
 par(mfrow = c(2, 2))
-l_ply(xvars, function(x) visreg(lar_c4_m3, xvar = x))
+l_ply(xvars, function(x) visreg(lar_dc3_m3, xvar = x))
 
 
 
 
-# . level plot ------------------------------------------------------------
-
-# plot predicted values from the model above on a level plot to show the
-# relations of LAR_C4 to soil moisture and PAR at a given temperature
-
-# predict values
-c4_envdf <- with(lar_data, expand.grid(s_logmoist = seq(-2.5, 2.5, length.out = 100),
-                                     s_logpar   = seq(-2.5, 2.5, length.out = 100),
-                                     s_temp     = median(s_temp),
-                                     co2        = c("amb", "elev"), 
-                                     ring = 1, id = "1:1", RY = "1:Year1"))
+# > subordinate c3  -------------------------------------------------------------
 
 
-c4_pred <- predict(lar_c4_m3, c4_envdf, re.form = NA)
-
-
-# reverse transform
-
-rev_ztrans <- function(x, xsd, xmean){
-  x * xsd + xmean
-}
-
-moist_sd <- sd(log(lar_data$Moist))
-moist_m  <- mean(log(lar_data$Moist))
-temp_sd  <- sd(lar_data$Temp)
-temp_m   <- mean(lar_data$Temp)
-par_sd   <- sd(log(lar_data$PAR))
-par_m    <- mean(log(lar_data$PAR))
-c4d_sd   <- sd(lar_data$lar_c4)
-c4d_m    <- mean(lar_data$lar_c4)
-
-c4_pred_df <- cbind(fit = c4_pred, c4_envdf) %>% 
-  mutate(r_moist = rev_ztrans(s_logmoist, xsd = moist_sd, xmean = moist_m),
-         r_temp  = rev_ztrans(s_temp, xsd = temp_sd, xmean = temp_m),
-         r_par   = rev_ztrans(s_logpar, xsd = par_sd, xmean = par_m),
-         r_fit   = rev_ztrans(fit, c4d_sd, c4d_m),
-         co2     = mapvalues(co2, c("amb", "elev"), c("Ambient", "eCO[2]")))
-
-lar_data_temp <- lar_data %>% 
-  mutate(co2     = mapvalues(co2, c("amb", "elev"), c("Ambient", "eCO[2]")))
-
-c4_levelplot <- ggplot(c4_pred_df, aes(x = r_moist, y = r_par)) + 
-  geom_tile(aes(fill = r_fit)) +
-  scale_fill_gradient2(expression(LAR[C4]), low = "blue",high = "red", mid = "white")+
-  stat_ellipse(data = lar_data_temp, aes(x = log(Moist), y = log(PAR), linetype = year), 
-               type = "norm", level = .7)+
-  geom_point(data = lar_data_temp, aes(x = log(Moist), y = log(PAR), shape = year), size = 2)+
-  scale_shape_manual("Year", values = c(0:2), label = 2013:2015)+
-  scale_linetype_manual("Year", values = c(1:3), label = 2013:2015)+
-  facet_grid(. ~ co2, labeller = label_parsed)+
-  labs(x = "ln(Moist)", y = expression(ln(PAR,~mu*mol~s^'-1'~m^"-2")))
-c4_levelplot
-
-
-
-
-# > LAR_C3 ------------------------------------------------------------------
-
-
-# test interaction
-lar_c3_m1 <- lmer(s_lar_c3 ~ co2 * (s_logmoist+s_temp+s_logpar) 
-                  + (1|ring) + (1|ring:plot) +(1|ring:year), data = lar_data)
-plot(lar_c3_m1)
-qqPlot(resid(lar_c3_m1))
-mcp.fnc(lar_c3_m1)
-oldf_c3 <- romr.fnc(lar_c3_m1, data = data.frame(lar_data))
-dplyr::setdiff(oldf_c3$data0, oldf_c3$data)
-  # one large outlier
-
-
-# remove the outlier
-lar_c3_m2 <- update(lar_c3_m1, data = oldf_c3$data)
-plot(lar_c3_m2)
-qqPlot(resid(lar_c3_m2))
-mcp.fnc(lar_c3_m2)
-
-dredge(lar_c3_m2, REML = F, fixed = c("co2", "s_logmoist", "s_temp", "s_logpar"))
-dredge(lar_c3_m2, REML = F)
-  # no interaction is suggsted
+# test interactions
+lar_sc3_m1 <- lmer(s_lar_sc3 ~ co2 * (s_logmoist + s_temp + s_logpar) + 
+                     (1|ring) + (1|ring:year) + (1|ring:plot), data = lar_data)
+plot(lar_sc3_m1)
+qqPlot(resid(lar_sc3_m1))
+lar_sc3_m1_full <- dredge(lar_sc3_m1, fixed = c("co2", "s_logmoist", "s_temp",  "s_logpar"))
+lar_sc3_m1_full
+# no interaction is indicated either way
 
 
 # test main effects
-lar_c3_m3 <- lmer(s_lar_c3 ~ co2 + s_logmoist + s_temp + s_logpar 
-                  + (1|ring) + (1|ring:plot) +(1|ring:year), 
-                  data = oldf_c3$data)
+lar_sc3_m2 <- lmer(s_lar_sc3 ~ co2 + s_logmoist+s_temp + s_logpar +
+                     (1|ring) + (1|ring:year) + (1|ring:plot), data = lar_data)
+
+
+# model diagnosis
+plot(lar_sc3_m2)
+qqPlot(resid(lar_sc3_m2))
 
 
 # get 95% and 90% CI for coefficients
-summary(lar_c3_m3)
-r.squaredGLMM(lar_c3_m3)
-confint(lar_c3_m3, method = "boot", nsim = 999)
-confint(lar_c3_m3, method = "boot", level = .9, nsim = 999)
+summary(lar_sc3_m2)
+r.squaredGLMM(lar_sc3_m2)
+confint(lar_sc3_m2, method = "boot", nsim = 999)
+confint(lar_sc3_m2, method = "boot", level = .9, nsim = 999)
 
 
 # get relative importance of each predictor from a models set composed of models
 # with all possible combination of each predictor
-lar_c3_m3_all <- dredge(lar_c3_m3, REML = F)
-lar_c3_m3_all
-importance(lar_c3_m3_all)
+lar_sc3_m2_all <- dredge(lar_sc3_m2, REML = F)
+lar_sc3_m2_all
+importance(lar_sc3_m2_all)
+# ensure the total number of occurence of each predictor is equal
 
 
 # partial regression plot
 par(mfrow = c(2, 2))
-l_ply(xvars, function(x) visreg(lar_c3_m3, xvar = x))
+l_ply(xvars, function(x) visreg(lar_sc3_m2, xvar = x))
+
+
+
+
+# > dominant c4  -------------------------------------------------------------
+
+
+# test interactions
+lar_dc4_m1 <- lmer(s_lar_dc4 ~ co2 * (s_logmoist + s_temp + s_logpar) + 
+                     (1|ring) + (1|ring:year) + (1|ring:plot), data = lar_data)
+lar_dc4_m1_full <- dredge(lar_dc4_m1, fixed = c("co2", "s_logmoist", "s_temp",  "s_logpar"))
+lar_dc4_m1_full
+plot(lar_dc4_m1)
+qqPlot(resid(lar_dc4_m1))
+# no interaction is suggested
+
+
+# test main effects
+lar_dc4_m2 <- lmer(s_lar_dc4 ~ co2 + s_logmoist+s_temp + s_logpar +
+                     (1|ring) + (1|ring:year) + (1|ring:plot), data = lar_data)
+
+
+# model diagnosis
+plot(lar_dc4_m2)
+qqPlot(resid(lar_dc4_m2))
+
+
+# get 95% and 90% CI for coefficients
+summary(lar_dc4_m2)
+r.squaredGLMM(lar_dc4_m2)
+confint(lar_dc4_m2, method = "boot", nsim = 999)
+confint(lar_dc4_m2, method = "boot", level = .9, nsim = 999)
+
+
+# get relative importance of each predictor from a models set composed of models
+# with all possible combination of each predictor
+lar_dc4_m2_all <- dredge(lar_dc4_m2, REML = F)
+lar_dc4_m2_all
+importance(lar_dc4_m2_all)
+# ensure the total number of occurence of each predictor is equal
+
+
+# partial regression plot
+par(mfrow = c(2, 2))
+l_ply(xvars, function(x) visreg(lar_dc4_m2, xvar = x))
+
+
+
+
+# > subordinate c4  -------------------------------------------------------------
+
+
+# test interactions
+lar_sc4_m1 <- lmer(s_lar_sc4 ~ co2 * (s_logmoist + s_temp + s_logpar) + 
+                     (1|ring) + (1|ring:year) + (1|ring:plot), data = lar_data)
+lar_sc4_m1_full <- dredge(lar_sc4_m1, fixed = c("co2", "s_logmoist", "s_temp",  "s_logpar"))
+lar_sc4_m1_full
+plot(lar_sc4_m1)
+qqPlot(resid(lar_sc4_m1))
+# no interaction is suggested
+
+# test main effects
+lar_sc4_m2 <- lmer(s_lar_sc4 ~ co2 + s_logmoist+s_temp + s_logpar +
+                     (1|ring) + (1|ring:year) + (1|ring:plot), data = lar_data)
+
+
+# model diagnosis
+plot(lar_sc4_m2)
+qqPlot(resid(lar_sc4_m2))
+
+
+# get 95% and 90% CI for coefficients
+summary(lar_sc4_m2)
+r.squaredGLMM(lar_sc4_m2)
+confint(lar_sc4_m2, method = "boot", nsim = 999)
+confint(lar_sc4_m2, method = "boot", level = .9, nsim = 999)
+
+
+# get relative importance of each predictor from a models set composed of models
+# with all possible combination of each predictor
+lar_sc4_m2_all <- dredge(lar_sc4_m2, REML = F)
+lar_sc4_m2_all
+importance(lar_sc4_m2_all)
+# ensure the total number of occurence of each predictor is equal
+
+
+# partial regression plot
+par(mfrow = c(2, 2))
+l_ply(xvars, function(x) visreg(lar_sc4_m2, xvar = x))
+
