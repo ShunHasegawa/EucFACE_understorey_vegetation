cd87e5479421876431bcca83b219655b048f115e
ShunHasegawa <hasegawashun.virgo@googlemail.com>
2017-12-01 16:10:35 +0900

    file names have been updated

--- R/Analysis.R
+++ R/Analysis.R
@@ -12,21 +12,20 @@ library(tidyr)
 library(lsmeans)
 library(visreg)
 library(MuMIn)
-library(LMERConvenienceFunctions)
 
 
 
 # load data ---------------------------------------------------------------
 
-graminoid_data <- read.csv("Data/graminoid_data.csv")  # data frame for graminoids
-forb_data      <- read.csv("Data/forb_data.csv")       # data frame for forbs
-site_var       <- c("year", "ring", "co2", "plot")              # vector for site variables
-site_data      <- graminoid_data[ ,site_var]                    # data frame for site
-SppName_gram   <- setdiff(names(graminoid_data), site_var)      # graminoid species names
-SppName_forb   <- setdiff(names(forb_data), site_var)           # forb species names
-env_data       <- read.csv("Data/env_data.csv")        # environmental variables (moisture, temperature and PAR)
-soil_data      <- read.csv("Data/soil_data.csv")       # soil nutrient data (nitrate, ammonium and phosphoate)
-sp_pfg         <- read.csv("Data/graminoid_pfg.csv")   # graminoid species and and their corresponding plant functional groups
+graminoid_data <- read.csv("Data/graminoid_data.csv")       # data frame for graminoids
+forb_data      <- read.csv("Data/forb_data.csv")            # data frame for forbs
+site_var       <- c("year", "ring", "co2", "plot")          # vector for site variables
+site_data      <- graminoid_data[ ,site_var]                # data frame for site
+SppName_gram   <- setdiff(names(graminoid_data), site_var)  # graminoid species names
+SppName_forb   <- setdiff(names(forb_data), site_var)       # forb species names
+env_data       <- read.csv("Data/env_data.csv")             # environmental variables (moisture, temperature and PAR)
+soil_data      <- read.csv("Data/soil_data.csv")            # soil nutrient data (nitrate, ammonium and phosphoate)
+sp_pfg         <- read.csv("Data/graminoid_pfg.csv")        # graminoid species and and their corresponding plant functional groups
 
 
 
@@ -35,5 +34,5 @@ sp_pfg         <- read.csv("Data/graminoid_pfg.csv")   # graminoid species and a
 
 options(na.action = "na.fail")      # change na.action setting for MuMIN::dredge
 source("R/analysis_diversity.R")    # analysis on diversity indices
-source("R/analysis_abundance.R")    # analysis on abundance of C3 and C4 graminoids for dominant and subordinate spp
+source("R/analysis_cover.R")        # analysis on cover of C3 and C4 graminoids for dominant and subordinate spp
 source("R/analysis_C43r_soilNP.R")  # multiple regression analysis on C4:C3 ratios and soil nutrients
